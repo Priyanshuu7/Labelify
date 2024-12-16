5fd8776ed07b2b9e75bb3d80e8c4069a1aa9c599
Priyanshu rajak <rajakpriyanshu581@gmail.com>
2024-12-16 22:27:11 +0530

    Add jwt auth

--- backend/src/routers/user.ts
+++ backend/src/routers/user.ts
@@ -1,8 +1,28 @@
 import { Router } from "express";
+import { PrismaClient } from "@prisma/client";
 import jwt from "jsonwebtoken";
 
+const prismaClient = new PrismaClient();
 const router = Router();
 
-router.post("/signin", (req, res) => {});
+const JWT_SECRET = "secret";
+
+router.post("/signin", async (req, res) => {
+  const hardcodedWalletAddress = "DZwSAUdxz8goAooy8rBdauQhERToKfGSwGm1PusydS7V";
+  const existingUser = await prismaClient.user.findFirst({
+    where: {
+      address: hardcodedWalletAddress,
+    },
+  });
+
+  if (existingUser) {
+    const token = jwt.sign(
+      {
+        userId: existingUser.id,
+      },
+      JWT_SECRET
+    );
+  }
+});
 
 export default router;
