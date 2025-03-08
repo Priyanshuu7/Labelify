35799dd9e230503cce4b83368d778b8d78b216a0
Priyanshuu7 <rajakpriyanshu581@gmail.com>
2025-03-08 23:41:32 +0530

    Update userfronetend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -12,5 +12,5 @@ app.use("/v1/worker", workerRouter);
 
 // Start the server and listen on port 3000
 app.listen(3000, () => {
-  console.log("Server started on port 3000"); // Log a message when the server starts
+    console.log("Server started on port 3000"); // Log a message when the server starts
 });
