d181f7fb3f8998cf00f50e6a638f14d003a02c6e
Priyanshuu7 <rajakpriyanshu581@gmail.com>
2025-03-09 10:57:57 +0530

    Update the index.js , middleware.js and types.js

--- backend/dist/middleware.js
+++ backend/dist/middleware.js
@@ -1,8 +1,12 @@
 "use strict";
 var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
+    return (mod && mod.__esModule)
+        ? mod
+        : {
+            "default": mod
+        };
 };
-Object.defineProperty(exports, "__esModule", { value: true });
+Object.defineProperty(exports, "__esModule", {value: true});
 exports.WorkerMiddleware = exports.authMiddleware = void 0;
 const jsonwebtoken_1 = __importDefault(require("jsonwebtoken"));
 const config_1 = require("./config");
@@ -10,10 +14,14 @@ const config_1 = require("./config");
 const authMiddleware = (req, res, next) => {
     var _a;
     // Extract the authorization header from the request
-    const authHeader = (_a = req.headers["authorization"]) !== null && _a !== void 0 ? _a : "";
+    const authHeader = (_a = req.headers["authorization"]) !== null && _a !== void 0
+        ? _a
+        : "";
     try {
         // Verify the JWT using the secret key
-        const decoded = jsonwebtoken_1.default.verify(authHeader, config_1.JWT_SECRET);
+        const decoded = jsonwebtoken_1
+            .default
+            .verify(authHeader, config_1.JWT_SECRET);
         //@ts-ignore
         if (decoded.userId) {
             // If userId is present in the decoded token, attach it to the request object
@@ -21,19 +29,17 @@ const authMiddleware = (req, res, next) => {
             req.userId = decoded.userId;
             // Proceed to the next middleware or route handler
             next();
-        }
-        else {
+        } else {
             // If userId is not present, respond with a 403 status
             res
                 .status(403)
-                .json({ message: "You are not logged in" });
+                .json({message: "You are not logged in"});
         }
-    }
-    catch (e) {
+    } catch (e) {
         // If token verification fails, respond with a 403 status
         res
             .status(403)
-            .json({ message: "You are not logged in" });
+            .json({message: "You are not logged in"});
     }
 };
 exports.authMiddleware = authMiddleware;
@@ -41,10 +47,14 @@ exports.authMiddleware = authMiddleware;
 const WorkerMiddleware = (req, res, next) => {
     var _a;
     // Extract the authorization header from the request
-    const authHeader = (_a = req.headers["authorization"]) !== null && _a !== void 0 ? _a : "";
+    const authHeader = (_a = req.headers["authorization"]) !== null && _a !== void 0
+        ? _a
+        : "";
     try {
         // Verify the JWT using the worker secret key
-        const decoded = jsonwebtoken_1.default.verify(authHeader, config_1.WORKER_JWT_SECRET);
+        const decoded = jsonwebtoken_1
+            .default
+            .verify(authHeader, config_1.WORKER_JWT_SECRET);
         //@ts-ignore
         if (decoded.userId) {
             // If userId is present in the decoded token, attach it to the request object
@@ -52,19 +62,17 @@ const WorkerMiddleware = (req, res, next) => {
             req.userId = decoded.userId;
             // Proceed to the next middleware or route handler
             next();
-        }
-        else {
+        } else {
             // If userId is not present, respond with a 403 status
             res
                 .status(403)
-                .json({ message: "You are not logged in" });
+                .json({message: "You are not logged in"});
         }
-    }
-    catch (e) {
+    } catch (e) {
         // If token verification fails, respond with a 403 status
         res
             .status(403)
-            .json({ message: "You are not logged in" });
+            .json({message: "You are not logged in"});
     }
 };
 exports.WorkerMiddleware = WorkerMiddleware;
