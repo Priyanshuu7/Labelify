908739d52b42dc34ba0ff2dadef41c5549fc7bf2
Priyanshuu7 <rajakpriyanshu581@gmail.com>
2025-04-08 13:16:07 +0530

    Update db.ts

--- backend/src/db.ts
+++ backend/src/db.ts
@@ -1,24 +1,26 @@
-import { PrismaClient } from "@prisma/client";
+import {PrismaClient} from "@prisma/client";
 
 const prismaClient = new PrismaClient();
 
-export const getNextTask = async (userId: number) => {
-    const task = await prismaClient.task.findFirst({
-        where: {
-            done: false,
-            submissions: {
-                none: {
-                    worker_id: userId
+export const getNextTask = async(userId : number) => {
+    const task = await prismaClient
+        .task
+        .findFirst({
+            where: {
+                done: false,
+                submissions: {
+                    none: {
+                        worker_id: userId
+                    }
                 }
+            },
+            select: {
+                id: true,
+                amount: true,
+                title: true,
+                options: true
             }
-        },
-        select: {
-            id: true,
-            amount: true,
-            title: true,
-            options: true
-        }
-    })
+        })
 
     return task
 }
