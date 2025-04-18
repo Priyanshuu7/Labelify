a96882de2cc8c5f172035c79587eb1c19e76c9a4
Priyanshuu7 <rajakpriyanshu581@gmail.com>
2025-04-19 04:47:34 +0530

    Completed user-frontend

--- User-Frontend/components/UploadImage.tsx
+++ User-Frontend/components/UploadImage.tsx
@@ -46,11 +46,11 @@ export function UploadImage({ onImageAdded, image }: {
 
     return <div>
         <div className="w-40 h-40 rounded border text-2xl cursor-pointer">
-                <div className="h-full flex justify-center flex-col relative w-full">
+                <div className="h-40 flex justify-center flex-col relative w-full">
                     <div className="h-full flex justify-center w-full pt-16 text-4xl">
                     {uploading ? <div className="text-sm">Loading...</div> : <>
                         +
-                        <input className="w-full h-full bg-red-400 w-40 h-40" type="file" style={{position: "absolute", opacity: 0, top: 0, left: 0, bottom: 0, right: 0, width: "100%", height: "100%"}} onChange={onFileSelect} />
+                        <input className="w-full h-full bg-red-400 " type="file" style={{position: "absolute", opacity: 0, top: 0, left: 0, bottom: 0, right: 0, width: "100%", height: "100%"}} onChange={onFileSelect} />
                     </>}
                 </div>
             </div>
