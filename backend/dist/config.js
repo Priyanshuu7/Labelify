79a2252ad01deda7ccff0b752ffdfb063369d16f
Priyanshuu7 <rajakpriyanshu581@gmail.com>
2025-05-02 18:13:49 +0530

    Updated config.ts

--- backend/dist/config.js
+++ backend/dist/config.js
@@ -1,7 +1,7 @@
 "use strict";
-var _a;
+var _a, _b;
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.TOTAL_DECIMALS = exports.WORKER_JWT_SECRET = exports.JWT_SECRET = void 0;
 exports.JWT_SECRET = (_a = process.env.JWT_SECRET) !== null && _a !== void 0 ? _a : "rajak123";
-exports.WORKER_JWT_SECRET = exports.JWT_SECRET + "worker";
+exports.WORKER_JWT_SECRET = (_b = process.env.WORKER_JWT_SECRET) !== null && _b !== void 0 ? _b : "worker123";
 exports.TOTAL_DECIMALS = 1000000;
--- backend/src/config.ts
+++ backend/src/config.ts
@@ -1,4 +1,4 @@
 export const JWT_SECRET = process.env.JWT_SECRET ?? "rajak123";
-export const WORKER_JWT_SECRET = JWT_SECRET + "worker";
+export const WORKER_JWT_SECRET = process.env.WORKER_JWT_SECRET ?? "worker123";
 export const TOTAL_DECIMALS = 1000_000;
 
