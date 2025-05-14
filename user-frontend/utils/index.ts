0bb6004ff623e031771a704ffd99024d8ad62b3c
Priyanshuu7 <rajakpriyanshu581@gmail.com>
2025-05-14 23:25:49 +0530

    Fix aws bugs

--- user-frontend/utils/index.ts
+++ user-frontend/utils/index.ts
@@ -1,3 +1,4 @@
+// this is the backend url //
 export const BACKEND_URL = "https://labelify.onrender.com";
 // Make sure to add your own cloudfront disrtibution url //
 export const CLOUDFRONT_URL = "https://dnwbvytj9ltty.cloudfront.net"
